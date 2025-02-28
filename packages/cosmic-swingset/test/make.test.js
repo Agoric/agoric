94355dc06b1ca9247e7d212e10580514193da0de
Siarhei Liakh <siarhei@agoric.com>
2025-02-28 13:04:54 -0500

    test(cosmic-swingset): add wallet provisioning test
    refs: #11062

--- packages/cosmic-swingset/test/make.test.js
+++ packages/cosmic-swingset/test/make.test.js
@@ -1,11 +1,12 @@
 // @ts-check
 import anyTest from 'ava';
-
+import fs from 'fs/promises';
 // Use ambient authority only in test.before()
 import { spawn as ambientSpawn } from 'child_process';
 import * as ambientPath from 'path';
 
 import { makeScenario2, pspawn } from './scenario2.js';
+import { stderr } from 'process';
 
 /** @type {import('ava').TestFn<Awaited<ReturnType<typeof makeTestContext>>>} */
 const test = anyTest;
@@ -74,3 +75,87 @@ test.serial('integration test: rosetta CI', async t => {
   ]);
   t.is(code, 0, 'make scenario2-run-rosetta-ci is successful');
 });
+
+/** @type {import('ava').Macro<[description: string, verifier?: any], any>} */
+const walletProvisioning = test.macro({
+  title(_, description, _verifier) {
+    return description;
+  },
+  async exec(t, _description, verifier) {
+    const retryCountMax = 5;
+    // Resume the chain... and concurrently, start a faucet AND run the rosetta-cli tests
+    const { pspawnAgd, scenario2 } = t.context;
+
+    // Run the chain until error or rosetta-cli exits.
+    const chain = scenario2.spawnMake(['scenario2-run-chain'], {
+      stdio: ['ignore', 'inherit', 'inherit'],
+    });
+    const fundPool = scenario2.spawnMake(['wait-for-cosmos', 'fund-provision-pool']);
+    const cleanup = async () => {
+      chain.kill();
+      fundPool.kill();
+      await Promise.allSettled([chain.exit, fundPool.exit]);
+    };
+    t.teardown(cleanup);
+
+    const fundPoolExitCode = await Promise.race([
+      fundPool.exit,
+      // Don't leave behind an unhandled rejection, but still treat winning this
+      // race as a failure.
+      chain.exit.then(c => `chain exited unexpectedly with code ${c}`),
+    ]);
+
+    t.is(fundPoolExitCode, 0, 'make fund-provision-pool is successful');
+
+    const soloAddr = await fs.readFile('t1/8000/ag-cosmos-helper-address', 'utf-8');
+
+    const checkWalletExists = async (address) => {
+      const { stdout, exit } = pspawnAgd(['query', '-ojson', 'vstorage', 'path', `published.wallet.${address}`]);
+      try {
+        await exit;
+      } catch (e) {
+        t.log(e);
+        return false;
+      }
+      t.log('query vstorage path published.wallet exits successfully');
+      const { value } = JSON.parse(stdout);
+      return !!value;
+    }
+
+    t.false(await checkWalletExists(soloAddr));
+
+    const provisionAcct = scenario2.spawnMake(['wait-for-cosmos', 'provision-acct', `ACCT_ADDR=${soloAddr}`]);
+
+    const cleanupProvisionAcct = async () => {
+      provisionAcct.kill();
+      await provisionAcct.exit;
+    };
+    t.teardown(cleanupProvisionAcct);
+
+    const provisionExitCode = await Promise.race([
+      provisionAcct.exit,
+      // Don't leave behind an unhandled rejection, but still treat winning this
+      // race as a failure.
+      chain.exit.then(c => `chain exited unexpectedly with code ${c}`),
+    ]);
+
+    t.is(provisionExitCode, 0, 'make provision-acct is successful');
+
+    // Wait for the wallet to be published
+    // XXX: This is a temporary solution to wait for the wallet to be published
+    // until we have a better way to do it.
+    let retryCount = 0;
+    while (retryCount < retryCountMax) {
+      if (await checkWalletExists(soloAddr)) {
+        t.pass('wallet is published');
+        break;
+      }
+      retryCount++;
+      const waitForCosmos = scenario2.spawnMake(['wait-for-cosmos']);
+      assert.equal(await waitForCosmos.exit, 0);
+    }
+    t.true(retryCount < retryCountMax, `wallet is published within ${retryCount} retries out of ${retryCountMax}`);
+  }
+});
+
+test.serial(walletProvisioning, 'wallet provisioning');
